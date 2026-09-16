7369cb8afc66eff7cad1a175fea50d2d73522a91
auggie05 <samaw0928@gmail.com>
2026-09-16 14:56:52 -0400

    Update general.lua

--- libraries/general.lua
+++ libraries/general.lua
@@ -3,3 +3,14 @@ ign=input.getNumber
 igb=input.getBool
 osn=output.setNumber
 osb=output.setBool
+
+--Returns true if there is a touch input within set paramaters.
+function touchWithin (x,y,rectX,rectY,rectW,rectH)
+    return x > rectX and y > rectY and x < rectX+rectW and y < rectY+rectH
+end
+
+--Rounds a number to the number of decimal places.
+function round (num, places)
+    mult = (10 ^ (places or 0))
+    return math.floor(num * mult + 0.5)/mult
+end
